85dcd3d24b7d09c064e2ff5b41b16389e8183afc
zkzkzhzj <zkzkzhzj@gmail.com>
2025-01-06 18:04:09 +0900

    chore: web change(react -> vue)

--- frontend/src/components/main/Main.js
+++
@@ -1,9 +0,0 @@
-function Main() {
-    return (
-        <div className="App">
-            메인 페이지 입니다.
-        </div>
-    );
-}
-
-export default Main;
--- frontend/src/components/map/MapMain.js
+++
@@ -1,9 +0,0 @@
-function MapMain() {
-    return (
-        <div className="App">
-            맵 페이지 입니다.
-        </div>
-    );
-}
-
-export default MapMain;

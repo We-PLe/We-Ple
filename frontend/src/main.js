45e3300abba5ba87883b0add44d03f795d1f739f
zkzkzhzj <zkzkzhzj@gmail.com>
2025-02-06 17:49:22 +0900

    chore: pinia 추가 및 AndD 주석 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,23 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+// 전역 상태 관리 라이브러리
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css';
+// And Design Vue
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 // createApp(App).use(router).mount('#app')
+const pinia = createPinia()
 const app = createApp(App);
 
+// pinia 사용
+app.use(pinia)
 // Ant Design Vue 사용
-app.use(router);
 app.use(Antd);
+app.use(router);
 app.mount('#app');
 
 // kakao login init
